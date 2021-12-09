97165ae6ebbb0fdbf688b21fc2a4330d2deeeb18
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-12-09 17:44:15 +0530

    Create signed unsigned int.c

---
+++ signed unsigned int.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    signed char i = 0;
+    for(;i>=0;i++);
+    printf("%d\n",i);
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned char i = -130;
+    for(;i>=0;i++);
+    printf("%d\n",i);
+    return 0;
+}
