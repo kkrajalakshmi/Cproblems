179de5f15db97856a48cd91587d49de30983032a
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-12-10 19:44:59 +0530

    Create size of string.c

---
+++ size of string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char arr[]="Data Engineer";
+    printf("The size of this character array is: %d",sizeof(arr));
+/*counts from 0th index to last index along with null character*/
+    return 0;
+}
