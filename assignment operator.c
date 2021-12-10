c9691b9142b27834b0180a5007f5ac9549673366
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-12-10 19:04:05 +0530

    Create assignment operator.c

---
+++ assignment operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y=5,z=5;
+    x+= y==z;
+    printf("X is : %d",x);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int x,y=5,z=5;
+    x= y==z;
+    printf("X is : %d",x);
+
+    return 0;
+}
